e12267dacf02edbdebe5a3a6940bea4def93e004
Alexander <alex17.05.84@yandex.ru>
2022-10-22 17:47:00 +0300

    Second App solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿
-Console.WriteLine("Введите чило 1 ");
-int a = Convert.Toin
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, являющееся квадратом первого числа ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b == a*a)
+{
+    Console.WriteLine("Правильно, это квадрат числа! ");
+}
+else 
+{
+    Console.WriteLine("Неверный ответ ");
+}
